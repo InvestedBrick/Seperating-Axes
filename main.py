24b20c1e4674135a4b6c7454100542139f246a44
InvestedBrick <julianbrecht25@gmail.com>
2025-03-01 21:12:32 +0100

    Some small changes

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ pygame.display.set_caption('Game test Prototype')
 
 from Constants import SCREEN_HEIGHT,SCREEN_WIDTH
 
-from Static_Object import *
+from Static_Objects import *
 
 from Player import *
 
@@ -17,8 +17,8 @@ def main():
     WALL_COLOR = (148, 93, 22)
     game = Game()
     player = Player( pygame.Rect((300,200,50,50)),(39, 51, 84))
-    ground = Static_Object( pygame.Rect((100,550,SCREEN_WIDTH-200,10)),WALL_COLOR)
-    wall1 = Static_Object( pygame.Rect((500,0,15,SCREEN_HEIGHT)),WALL_COLOR)
+    ground = Static_Rect( pygame.Rect((100,550,SCREEN_WIDTH-200,10)),WALL_COLOR)
+    wall1 = Static_Rect( pygame.Rect((500,0,15,SCREEN_HEIGHT)),WALL_COLOR)
     game.insert_moving_object(player)
     game.insert_static_object(ground)
     game.insert_static_object(wall1)
