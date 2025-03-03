d9877dfd39be955b0bb3c00c3d04d2cc42653c63
InvestedBrick <julianbrecht25@gmail.com>
2025-03-03 14:09:01 +0100

    Added more objects

--- main.py
+++ main.py
@@ -15,14 +15,22 @@ from Game import *
 
 def main():
     WALL_COLOR = (148, 93, 22)
+    PLAYER_COLOR = (39, 51, 84)
     game = Game()
-    player = Player( pygame.Rect((300,0,50,50)),(39, 51, 84))
-    #ground = Static_Rect( pygame.Rect((100,450,SCREEN_WIDTH-200,10)),WALL_COLOR)
+    player = Player( pygame.Rect((300,0,50,50)),PLAYER_COLOR)
+    ground = Static_Rect( pygame.Rect((100,SCREEN_HEIGHT-200,SCREEN_WIDTH-200,10)),WALL_COLOR)
+    pent = Static_Convex_Polygon([
+        (910, 100),   # Top center point
+        (310, 800),   # Bottom left point
+        (910, 950),   # Bottom center point
+        (1510, 800),  # Bottom right point
+        (1510, 300)   # Upper right point
+    ],WALL_COLOR)
     #wall1 = Static_Rect( pygame.Rect((200,0,15,SCREEN_HEIGHT)),WALL_COLOR)
     slope = Static_Convex_Polygon([(0,SCREEN_HEIGHT),(SCREEN_WIDTH,SCREEN_HEIGHT),(SCREEN_WIDTH,300)],WALL_COLOR)
     game.insert_moving_object(player)
-    #game.insert_static_object(ground)
-    #game.insert_static_object(wall1)
+    game.insert_static_object(ground)
+    game.insert_static_object(pent)
     game.insert_static_object(slope)
     game.loop()
     
