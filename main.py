34fec1b160bc82ebcd1f2d7e7ca7aa21e78567b1
InvestedBrick <julianbrecht25@gmail.com>
2025-03-04 20:30:54 +0100

    One must imagine Sisyphus happy

--- main.py
+++ main.py
@@ -4,26 +4,27 @@
 import pygame
 pygame.init()
 pygame.display.set_caption('Game test Prototype')
-
+import random
 from Constants import SCREEN_HEIGHT,SCREEN_WIDTH
 
-from Static_Objects import *
+from Static_Objects import Static_Rect,Static_Convex_Polygon
+from Dynamic_Objects import Dynamic_Convex_Polygon
+from Player import Player
 
-from Player import *
+from Game import Game
 
-from Game import *
-
+from utils import regular_polygon
 def main():
     WALL_COLOR = (148, 93, 22)
     PLAYER_COLOR = (39, 51, 84)
     game = Game()
     player = Player( pygame.Rect((300,0,50,50)),PLAYER_COLOR)
     ground = Static_Rect( pygame.Rect((100,SCREEN_HEIGHT-400,SCREEN_WIDTH-200,10)),WALL_COLOR)
-    wall1 = Static_Rect( pygame.Rect((200,0,15,SCREEN_HEIGHT)),WALL_COLOR)
-    slope = Static_Convex_Polygon([(0,SCREEN_HEIGHT),(SCREEN_WIDTH,SCREEN_HEIGHT),(SCREEN_WIDTH,300)],WALL_COLOR)
-    game.insert_moving_object(player)
-    game.insert_static_object(ground)
-    #game.insert_static_object(wall1)
+    boulder = Dynamic_Convex_Polygon( regular_polygon(8,20,(SCREEN_WIDTH - 400,430)),WALL_COLOR,(SCREEN_WIDTH - 400,430))
+    slope = Static_Convex_Polygon([(0,SCREEN_HEIGHT),(SCREEN_WIDTH,SCREEN_HEIGHT),(SCREEN_WIDTH,400)],WALL_COLOR)
+    game.insert_dynamic_object(player)
+    #game.insert_static_object(ground)
+    game.insert_dynamic_object(boulder)
     game.insert_static_object(slope)
     game.loop()
     
